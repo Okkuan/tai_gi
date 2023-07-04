4aa55d0ca5d2a2d08939b9c218eb402115ae278f
DESKTOP-7N4FING\ace <k24258697@gmail.com>
2023-07-04 14:30:57 +0800

    NEW LOGIN

--- src/App.js
+++ src/App.js
@@ -1,30 +1,72 @@
-import styled from 'styled-components'
-import HomePage from './HomePage'
-import RecordingPage from './RecordingPage';
-import AchievementPage from './AchievementPage'
-import NewProjectPage from './NewProjectPage'
+import styled from "styled-components";
+import HomePage from "./HomePage";
+import RecordingPage from "./RecordingPage";
+import AchievementPage from "./AchievementPage";
+import NewProjectPage from "./NewProjectPage";
+import LoginPage from "./LoginPage";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react"
+import { useState } from "react";
 
-
-
-const Container = styled.div`
-`
+const Container = styled.div``;
 
 function App() {
-    const [currentProject, setCurrentProject] = useState([])
-    return (
-        <Container>
-            <HashRouter>
-                <Routes>
-                <Route exact path="*" element={<HomePage currentProject={currentProject} setCurrentProject={setCurrentProject}/>}/>
-                <Route exact path="NewProject" element={<NewProjectPage currentProject={currentProject} setCurrentProject={setCurrentProject}/>}/>
-                <Route exact path="Recording" element={<RecordingPage currentProject={currentProject} setCurrentProject={setCurrentProject}/>}/>
-                <Route exact path="Achievement" element={<AchievementPage currentProject={currentProject} setCurrentProject={setCurrentProject}/>}/>
-                </Routes>
-            </HashRouter>
-        </Container>
-    );
+  const [currentProject, setCurrentProject] = useState([]);
+  return (
+    <Container>
+      <HashRouter>
+        <Routes>
+          <Route
+            exact
+            path="Login"
+            element={
+              <LoginPage
+              />
+            }
+          />
+          <Route
+            exact
+            path="*"
+            element={
+              <HomePage
+                currentProject={currentProject}
+                setCurrentProject={setCurrentProject}
+              />
+            }
+          />
+          <Route
+            exact
+            path="NewProject"
+            element={
+              <NewProjectPage
+                currentProject={currentProject}
+                setCurrentProject={setCurrentProject}
+              />
+            }
+          />
+          <Route
+            exact
+            path="Recording"
+            element={
+              <RecordingPage
+                currentProject={currentProject}
+                setCurrentProject={setCurrentProject}
+              />
+            }
+          />
+          <Route
+            exact
+            path="Achievement"
+            element={
+              <AchievementPage
+                currentProject={currentProject}
+                setCurrentProject={setCurrentProject}
+              />
+            }
+          />
+        </Routes>
+      </HashRouter>
+    </Container>
+  );
 }
 
 export default App;
